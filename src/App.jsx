0e0bd9b6a023b885f6159cc175222df798978a6d
CristianF09 <cris7i_laurentiu@yahoo.com>
2024-09-22 14:14:53 +0300

    Update File

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import FeedbackOptions from './components/FeedbackOptions';
+import Statistics from './components/Statistics';
+import Section from './components/Section';
+import styles from './App.module.css'; 
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleFeedback = (type) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = feedback;
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    const { good } = feedback;
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
+
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+
+  return (
+    <div className={styles.app}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <p className={styles.notification}>There is no feedback</p>
+        )}
+      </Section>
+    </div>
+  );
+};
+
+export default App;
